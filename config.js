7110f134d8ef5025eaba6e9954bd62a5c8f0b613
Jae Wilson <jaewilson07@gmail.com>
2020-12-31 18:14:06 +0100

    first push

---
+++ config.js
@@ -0,0 +1,82 @@
+const config = {
+  gatsby: {
+    pathPrefix: '/',
+    siteUrl: 'https://www.onyxreporting.com',
+    gaTrackingId: null,
+    trailingSlash: false,
+  },
+  header: {
+    logo:
+      'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a25e76ae-39d5-424d-9719-657db0de0a41/onyx_logo.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201231%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201231T153900Z&X-Amz-Expires=86400&X-Amz-Signature=14f26cb320592d6494b8897a469b5fea6130825a25e7ec50e93a190a971cee42&X-Amz-SignedHeaders=host',
+    logoLink: 'http://www.onyxrpeorting.com',
+    title:
+      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    helpUrl: '',
+    tweetText: '',
+    social: `<li>
+		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
+		      </div>
+		    </a>
+		  </li>
+			<li>
+		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
+		      <div class="discordBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+		      </div>
+		    </a>
+		  </li>`,
+    links: [{ text: '', link: '' }],
+    search: {
+      enabled: false,
+      indexName: '',
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+    },
+  },
+  sidebar: {
+    forcedNavOrder: [
+      '/introduction', // add trailing slash if enabled above
+      '/codeblock',
+    ],
+    collapsedNav: [
+      '/codeblock', // add trailing slash if enabled above
+    ],
+    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
+    frontline: false,
+    ignoreIndex: true,
+    title:
+      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+  },
+  siteMetadata: {
+    title: 'Gatsby Gitbook Boilerplate | Hasura',
+    description: 'Documentation built with mdx. Powering hasura.io/learn ',
+    ogImage: null,
+    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
+    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+  },
+  pwa: {
+    enabled: false, // disabling this will also remove the existing service worker.
+    manifest: {
+      name: 'Gatsby Gitbook Starter',
+      short_name: 'GitbookStarter',
+      start_url: '/',
+      background_color: '#6b37bf',
+      theme_color: '#6b37bf',
+      display: 'standalone',
+      crossOrigin: 'use-credentials',
+      icons: [
+        {
+          src: 'src/pwa-512.png',
+          sizes: `512x512`,
+          type: `image/png`,
+        },
+      ],
+    },
+  },
+};
+
+module.exports = config;
---
+++ content/posts.md
@@ -0,0 +1,11 @@
+---
+title: 'Posts'
+metaTitle: 'Syntax Highlighting is the meta title tag for this page'
+metaDescription: 'This is the meta description for this page'
+---
+
+This is your Posts page
+
+```javascript
+import React from 'react';
+```
